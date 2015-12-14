27c0d007d9171b5dc3bda72bc7eda0dca15c254a
Dakota Polenz <polenzdakota@yahoo.com>
2015-12-13 23:23:55 -0800

    Added needed script files

---
+++ Wedonthavejack/Assets/Scripts/IRobot.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IRobot : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
