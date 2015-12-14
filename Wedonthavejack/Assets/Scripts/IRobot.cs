4937a365898e54dc0a567de13aa63ad78e6ff4df
Dakota Polenz <polenzdakota@yahoo.com>
2015-12-13 23:30:01 -0800

    Updated interface for robot scripts

--- Wedonthavejack/Assets/Scripts/IRobot.cs
+++ Wedonthavejack/Assets/Scripts/IRobot.cs
@@ -1,15 +1,64 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class IRobot : MonoBehaviour {
+/// <summary>
+/// Interface for each robot game object.
+/// </summary>
+public interface IRobot {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+	/// <summary>
+	/// Moves the towards target.
+	/// </summary>
+	void MoveTowardsTarget();
+
+	/// <summary>
+	/// Gets the collision.
+	/// </summary>
+	/// <returns><c>true</c>, if collision was gotten, <c>false</c> otherwise.</returns>
+	bool GetCollision();
+
+	/// <summary>
+	/// Sets the initial position.
+	/// </summary>
+	void SetInitialPosition();
+
+	/// <summary>
+	/// Moves the robot forward.
+	/// </summary>
+	/// <returns><c>true</c>, if forward was moved, <c>false</c> otherwise.</returns>
+	bool MoveForward();
+
+	/// <summary>
+	/// Rotates the robot right.
+	/// </summary>
+	void RotateRight();
+
+	/// <summary>
+	/// Rotates the robot left.
+	/// </summary>
+	void RotateLeft();
+
+	/// <summary>
+	/// Updates the sprite.
+	/// </summary>
+	void UpdateSprite();
+
+	/// <summary>
+	/// Gets the position.
+	/// </summary>
+	/// <returns>The position.</returns>
+	Vector3 GetPosition();
+
+	/// <summary>
+	/// Gets the rotation.
+	/// </summary>
+	/// <returns>The rotation.</returns>
+	int[] GetRotation();
+
+	/// <summary>
+	/// Sets the rotation.
+	/// </summary>
+	/// <param name="rotation">Rotation.</param>
+	void SetRotation(int[] rotation);
+
 }
