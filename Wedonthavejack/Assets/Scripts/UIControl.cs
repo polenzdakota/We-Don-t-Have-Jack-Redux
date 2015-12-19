25c03694e7c3a3eb096794a789d565c8c7634c15
Dakota Polenz <polenzdakota@yahoo.com>
2015-12-19 07:39:43 -0800

    Updated UIControl and linked test buttons

--- Wedonthavejack/Assets/Scripts/UIControl.cs
+++ Wedonthavejack/Assets/Scripts/UIControl.cs
@@ -1,15 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class UIControl : MonoBehaviour {
+	public GameObject actionList;
+	public GameObject actionButton;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+	public void ButtonPress(string strAction) {
+		Vector3 iniVec = new Vector3 (0, 0, 0);
+		Quaternion iniQuat = new Quaternion (0, 0, 0, 0);
+		GameObject newButton = Instantiate (actionButton, iniVec, iniQuat) as GameObject;
+		newButton.transform.SetParent (actionList.transform);
+		newButton.GetComponentInChildren<Text>().text = strAction;
 	}
 }
