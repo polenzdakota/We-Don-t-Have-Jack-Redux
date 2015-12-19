a469e49ea7bd58afd5641d19f6dc000e7c17fd5c
Dakota Polenz <polenzdakota@yahoo.com>
2015-12-19 10:34:25 -0800

    Updated commads

--- Wedonthavejack/Assets/Scripts/Commands.cs
+++ Wedonthavejack/Assets/Scripts/Commands.cs
@@ -1,15 +1,31 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Commands : MonoBehaviour {
+	public GameObject currentRobot;
+	private IList<IAction> actions;
 
 	// Use this for initialization
 	void Start () {
-	
+		actions = new List<IAction> ();
+		currentRobot = GameObject.FindGameObjectWithTag ("Player");
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	public void AddAction(IAction addedAction) {
+		actions.Add (addedAction);
+	}
+
+	public void ExecuteActions() {
+		float secToWait = 0.5f;
+		foreach (IAction action in actions) {
+			StartCoroutine(TimeThenAction(secToWait, action));
+			secToWait += 0.5f;
+		}
+	}
+
+	IEnumerator TimeThenAction(float sec, IAction action) {
+		yield return new WaitForSeconds (sec);
+		action.InvokeAction ();
 	}
 }
