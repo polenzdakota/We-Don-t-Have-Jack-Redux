8493b3884d19f53c0e4b1da387dd28109030b0b2
Dakota Polenz <polenzdakota@yahoo.com>
2015-12-19 10:36:42 -0800

    Updated documentation for commands

--- Wedonthavejack/Assets/Scripts/Commands.cs
+++ Wedonthavejack/Assets/Scripts/Commands.cs
@@ -2,6 +2,10 @@
 using System.Collections;
 using System.Collections.Generic;
 
+/// <summary>
+/// Script contains all commands in a list and includes methods for
+/// managing and executing said commands.
+/// </summary>
 public class Commands : MonoBehaviour {
 	public GameObject currentRobot;
 	private IList<IAction> actions;
@@ -12,10 +16,17 @@ public class Commands : MonoBehaviour {
 		currentRobot = GameObject.FindGameObjectWithTag ("Player");
 	}
 
+	/// <summary>
+	/// Adds the action to the command list.
+	/// </summary>
+	/// <param name="addedAction">Added action.</param>
 	public void AddAction(IAction addedAction) {
 		actions.Add (addedAction);
 	}
 
+	/// <summary>
+	/// Executes all actions in the list in 0.5 second intervals.
+	/// </summary>
 	public void ExecuteActions() {
 		float secToWait = 0.5f;
 		foreach (IAction action in actions) {
@@ -24,6 +35,12 @@ public class Commands : MonoBehaviour {
 		}
 	}
 
+	/// <summary>
+	/// Waits the given amount of time then executes the action.
+	/// </summary>
+	/// <returns>The then action.</returns>
+	/// <param name="sec">Sec.</param>
+	/// <param name="action">Action.</param>
 	IEnumerator TimeThenAction(float sec, IAction action) {
 		yield return new WaitForSeconds (sec);
 		action.InvokeAction ();
