37ecce709b805609e9f6db28f510ad32e36ad60d
Dakota Polenz <polenzdakota@yahoo.com>
2015-12-23 04:57:06 -0800

    Added action scritpts and stubs. Updated to Unity 5.3.1

---
+++ Wedonthavejack/Assets/Scripts/Actions/MainMoveForward.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMoveForward : MonoBehaviour, IAction {
+
+    /// <summary>
+    /// Moves the robot forward
+    /// </summary>
+    /// <returns></returns>
+    public bool InvokeAction() {
+        return false;
+    }
+
+    /// <summary>
+    /// Undos the action
+    /// </summary>
+    /// <returns></returns>
+    public bool UndoAction() {
+        return false;
+    }
+}
---
+++ Wedonthavejack/Assets/Scripts/Actions/MainRotateLeft.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainRotateLeft : MonoBehaviour, IAction {
+
+    /// <summary>
+    /// Rotates the robot left
+    /// </summary>
+    /// <returns></returns>
+    public bool InvokeAction() {
+        return false;
+    }
+
+    /// <summary>
+    /// Undos the action
+    /// </summary>
+    /// <returns></returns>
+    public bool UndoAction() {
+        return false;
+    }
+}
